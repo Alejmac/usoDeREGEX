c3a5396450d3feda8a9e320efe0997e040a404f1
Alejandro <alejmac2508@gmail.com>
2023-10-09 21:23:34 -0600

    ejemplo 1

---
+++ regex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string testString = "John Doe, 42 years";
+        Regex regex = new Regex(@"^([^,]+),\s([0-9]+)");
+        Match match = regex.Match(testString);
+        if (match.Success)
+            Console.WriteLine("Name: " + match.Groups[1].Value + ". Age: " + match.Groups[2].Value);
+    }
+}
